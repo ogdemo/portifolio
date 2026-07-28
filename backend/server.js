c7e4342ba86ad2ec0031e04209e4edd02db5c3dd
demo <ogdemo23@gmail.com>
2026-07-28 20:56:13 +0300

    still commit

--- backend/server.js
+++ backend/server.js
@@ -417,14 +417,11 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 4000,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
-
-db.connect((err) => {
-  if (err) console.log("DB ERROR", err);
-  else console.log("DB Connected");
-});
-
 // REGISTER
 app.post("/register", (req, res) => {
   const { fullname, email, password, phone, location } = req.body;
